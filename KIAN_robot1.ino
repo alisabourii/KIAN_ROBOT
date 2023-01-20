9aecca4cc4aa8be9e0ed2cc752a79fc35981a978
Ali Sabouri <72344723+alisabourii@users.noreply.github.com>
2023-01-20 09:30:27 +0300

    KIAN robot Version 1

---
+++ KIAN_robot1.ino
@@ -0,0 +1,54 @@
+const int Enable_A = 10;
+const int Enable_B = 11;
+const int inputA1 = 3;
+const int inputA2 = 5;
+const int inputB1 = 6;
+const int inputB2 = 9;
+
+void setup() {
+  pinMode(Enable_A, OUTPUT);
+  pinMode(Enable_B, OUTPUT);
+  pinMode(inputA1, OUTPUT);
+  pinMode(inputA2, OUTPUT);
+  pinMode(inputB1, OUTPUT);
+  pinMode(inputB2, OUTPUT);
+
+}
+
+void loop() {
+  hareket_digital();
+
+}
+
+
+void hareket_digital()
+{
+  digitalWrite(Enable_A, HIGH);
+  digitalWrite(Enable_B, HIGH);
+  digitalWrite(inputA1, HIGH);
+  digitalWrite(inputA2, LOW);
+  
+  digitalWrite(inputB1 , HIGH);
+  digitalWrite(inputB2, LOW);
+  delay(2000);
+  digitalWrite(Enable_A, LOW);
+  digitalWrite(Enable_B, LOW);
+  delay(1000);
+  digitalWrite(Enable_A, HIGH);
+  digitalWrite(Enable_B, HIGH);
+  
+  digitalWrite(inputA1, LOW);
+  digitalWrite(inputA2, HIGH);
+  
+  digitalWrite(inputB1 , LOW);
+  digitalWrite(inputB2, HIGH);
+  delay(2000);
+  digitalWrite(Enable_A, LOW);
+  digitalWrite(Enable_B, LOW);
+  delay(1000);
+  }
+
+
+
+
+  
